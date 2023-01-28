d15ccefa741760acb76b89f31b8e29554fc4855d
Roman <spiderr.roma@gmail.com>
2023-01-28 20:05:04 +0200

    added exceptions but the scripts are still dxecute because text exists

--- script.py
+++ script.py
@@ -2,21 +2,26 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-name = "Drive" #define the name of the script you want to download
+name = "Boondock Saints" #define the name of the script you want to download
 url = "https://imsdb.com/scripts/" + name + ".html"
 folder = "scripts" #define the folder you want to save your scripts to
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
+except requests.exceptions.RequestException as err:
+    print ("Somethinf went wrong", err)
 # Parse the HTML content
 soup = BeautifulSoup(response.content, "html.parser")
 
 elements = soup.find_all(class_="scrtext")
-
+if not elements:
+    raise Exception("No script by the following url not found")
+else:
 # Extract the text from the elements
-text = [element.get_text() for element in elements]
+    text = [element.get_text() for element in elements]
 
 # Join the text into a single string
-text = "\n".join(text)
+    text = "\n".join(text)
 
 #create and save to folder
 if not os.path.exists(folder):
